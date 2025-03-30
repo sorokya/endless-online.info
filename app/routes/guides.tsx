e6e6cf7fd816a153303e467a3b2d9a845c0dd1ac
Richard Leek <richard@richardleek.com>
2025-03-30 20:37:04 +0200

    Add temp guides page

---
+++ app/routes/guides.tsx
@@ -0,0 +1,9 @@
+export default function Guides() {
+  return (
+    <div className="container mx-auto p-6">
+      <div className="card bg-base-200 p-4 shadow-xl">
+        <div className="card-body text-center">Coming soon!</div>
+      </div>
+    </div>
+  );
+}
